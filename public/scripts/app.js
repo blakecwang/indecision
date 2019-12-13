c68ec90acd22b49a76f928291768f26b08f56417
Blake Wang <bwang@intouchhealth.com>
2019-12-13 12:14:20 -0800

    Add some conditional logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,81 @@
-"use strict";
+'use strict';
 
 console.log('app.js is running');
+
+var app = {
+  title: 'Indecision',
+  subtitle: 'But how to choose?',
+  options: ['One', 'Two'],
+  nothing: 'nowhere'
+};
+
 var template = React.createElement(
-  "p",
+  'div',
   null,
-  "Did this change?"
+  React.createElement(
+    'h1',
+    null,
+    app.title.toUpperCase()
+  ),
+  app.subtitle && React.createElement(
+    'h2',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options:' : 'No options!'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'item 2'
+    )
+  )
 );
+
+var user = {
+  name: 'Blake Wang',
+  //  age: 21,
+  location: 'Santa Barbara'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name.toUpperCase() : 'ANONYMOUS'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
 console.log('app.js is running')
-var template = <p>Did this change?</p>;
+
+var app = {
+  title: 'Indecision',
+  subtitle: 'But how to choose?',
+  options: ['One', 'Two'],
+  nothing: 'nowhere'
+}
+
+var template = (
+  <div>
+    <h1>{app.title.toUpperCase()}</h1>
+    {app.subtitle && <h2>{app.subtitle}</h2>}
+    <p>{app.options.length > 0 ? 'Here are your options:' : 'No options!'}</p>
+    <ol>
+      <li>item 1</li>
+      <li>item 2</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Blake Wang',
+//  age: 21,
+  location: 'Santa Barbara'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name.toUpperCase() : 'ANONYMOUS'}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);

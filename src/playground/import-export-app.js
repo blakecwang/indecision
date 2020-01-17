0d848905e8f20eaa730289bb1ca93bd90e9a6be3
Blake Wang <bwang@intouchhealth.com>
2020-01-16 16:33:02 -0800

    Learn about import/export

---
+++ src/playground/import-export-app.js
@@ -0,0 +1,6 @@
+import utilsSubtract, { square, add } from './utils.js';
+
+console.log('app.js is NOW running');
+console.log(square(5));
+console.log(add(5, 10));
+console.log(utilsSubtract(100, 20));
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// https://webpack.js.org/concepts/
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
